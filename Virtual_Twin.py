c0c1f69004d13294e194b89acffdd163e51acb20
hinchy-18 <sh3g21@soton.ac.uk>
2025-02-16 12:45:08 +0000

    fg

--- Virtual_Twin.py
+++ Virtual_Twin.py
@@ -1,6 +1,16 @@
+###############################################
+#                   IMPORTS                   #
+###############################################
+
 import numpy as np
 import pybullet as p
 import time
+
+
+
+###############################################
+#                  FUNCTIONS                  #
+###############################################
 
 def findJointsToAnimate():
     #prints the name and type of each joint and its index, not necessarily the index that the joints were created in
@@ -9,48 +19,77 @@ def findJointsToAnimate():
     for i in range(numJoints):
         jointInfo = p.getJointInfo(gantry, i)
         print("Joint index:", jointInfo[0], "Joint name:", jointInfo[1].decode("utf-8"), "Joint type:", jointInfo[2])
-def animateSystem():
-    while True:
-        t = time.time()
-        targetPosn = 1.2*np.sin(t)
+def defineMovement(ts):
+    yaxisPos = 1.2*np.sin(ts)
+    zaxisPos = 0.5*np.sin(ts)
+    gimbalFramePos = 0.2*np.sin(ts)
+
+    return yaxisPos, zaxisPos, gimbalFramePos
+def animateSystem(yaxisPos, zaxisPos, gimbalFramePos, tmax, dt):
+    for i in range(tmax):
 
         #move y-axis in x direction
         p.setJointMotorControl2(bodyUniqueId=gantry,
                                 jointIndex=yaxisPrismaticJointIndex,
                                 controlMode=p.POSITION_CONTROL,
-                                targetPosition=targetPosn)
+                                targetPosition=yaxisPos[i])
 
         #move z-axis in y direction
         p.setJointMotorControl2(bodyUniqueId=gantry,
                                 jointIndex=zaxisPrismaticJointIndex,
                                 controlMode=p.POSITION_CONTROL,
-                                targetPosition=targetPosn)
+                                targetPosition=zaxisPos[i])
 
         #move gimbal frame in z direction
         p.setJointMotorControl2(bodyUniqueId=gantry,
                                 jointIndex=gimbalFramePrismaticJointIndex,
                                 controlMode=p.POSITION_CONTROL,
-                                targetPosition=targetPosn)
-
+                                targetPosition=gimbalFramePos[i])
 
         p.stepSimulation()
-        time.sleep(1/240)
+        time.sleep(dt)
+
+
+
+###############################################
+#             VARIABLE DEFINITIONS            #
+###############################################
+
+#define time the same way it was done in the dynamic model to make future integration easier
+tmax = 10 #seconds
+tspan = np.array([0, tmax])
+dt = 0.01
+t_eval = np.arrange(tspan[0], tspan[1]+dt, dt) #this is the array of times we will use
+
+
+###############################################
+#                PYBULLET SETUP               #
+###############################################
 
 p.connect(p.GUI)
 p.setGravity(0,0,-9.81)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0) #not showing GUI to make animations look nicer
 
 gantry = p.loadURDF("Gantry.urdf", basePosition=np.array([0,0,0]))
 
+
+###############################################
+#                 PROCESSING                  #
+###############################################
+
+#first assign the correct joint number to each joint
 findJointsToAnimate()
 yaxisPrismaticJointIndex = 6
 zaxisPrismaticJointIndex = 7
 gimbalFramePrismaticJointIndex = 8
 
-animate = True
+#then get the positions of the links over time
+yaxisPos, zaxisPos, gimbalFramePos = defineMovement(t_eval)
+
+animate = True #do we want to animate (control), or move with mouse
 
 if animate:
-    animateSystem()
+    animateSystem(yaxisPos, zaxisPos, gimbalFramePos, tmax, dt)
 else:
     p.setRealTimeSimulation(1)
     while True:
